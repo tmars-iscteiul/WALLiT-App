5ada4fa9c6a7b6d80693af70775a5dc1f72aa79d
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2019-03-24 22:57:30 +0000

    Adjusted login and menu screens

--- app/src/main/java/com/example/wallit_app/OperationActivity.java
+++ app/src/main/java/com/example/wallit_app/OperationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
 import android.support.v7.widget.Toolbar;
 
 public class OperationActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class OperationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_operation);
+        setContentView(R.layout.operation_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -21,9 +22,13 @@ public class OperationActivity extends AppCompatActivity {
         String message = intent.getStringExtra(LoginActivity.LOGIN_USER);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.welcome_text);
         textView.setText("Welcome " + message + "!");
 
+
+    }
+
+    public void logoutUser(View view)   {
 
     }
 
