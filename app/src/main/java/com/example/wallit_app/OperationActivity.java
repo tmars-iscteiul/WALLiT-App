446f2aec8ff24f2ba987b5d6fed2a0a079a54109
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2019-03-24 23:47:21 +0000

    Added operations screen (no functionality yet)

--- app/src/main/java/com/example/wallit_app/OperationActivity.java
+++ app/src/main/java/com/example/wallit_app/OperationActivity.java
@@ -3,6 +3,8 @@ package com.example.wallit_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
@@ -24,8 +26,28 @@ public class OperationActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.welcome_text);
         textView.setText("Welcome " + message + "!");
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void logoutUser(View view)   {
