1dfb193ac1c2e0adc05b9fb6636f2f22fec4ced7
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2019-05-10 13:50:21 +0100

    Updated movement history table's design

--- app/src/main/java/com/example/wallit_app/StatsActivity.java
+++ app/src/main/java/com/example/wallit_app/StatsActivity.java
@@ -1,9 +1,14 @@
 package com.example.wallit_app;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.levitnudi.legacytableview.LegacyTableView;
+
 public class StatsActivity extends ToolBarActivity {
+
+    private Resources res = getResources();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +17,19 @@ public class StatsActivity extends ToolBarActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
+
+    /*
+    If deposit row: change font color to #6699CC
+    If withdraw row: change font color to #676767
+    Date, (Wit. / Dep.), Amount, Balance.
+
+
+    int id = res.getIdentifier("titleText", "id", getContext().getPackageName());
+
+    TODO: Add page system, so user can see entire history of movements. (9 entries per page)
+     */
 
 }
